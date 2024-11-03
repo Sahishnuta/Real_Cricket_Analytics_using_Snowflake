a38dee07709941c6bbee629173d9ccf4380a65a3
Sahishnuta <50331193+Sahishnuta@users.noreply.github.com>
2024-11-03 10:51:24 +0100

    Add files via upload

--- 07_delivery_fact_table.sql
+++ 07_delivery_fact_table.sql
@@ -19,22 +19,51 @@ where d.match_type_number = 4686;
 select
 	d.match_type_number as match_id,
 	td.team_id, td.team_name,
-	
+	bpd.player_id as bowler_id, bpd.player_name,
+	spd.player_id as batter_id, spd.player_name,
+	nspd.player_id as non_stricker_id, nspd.player_name,
 from cricket.clean.delivery_clean_tbl d
-join team_dim td on d.team_name = td.team_name
+	join team_dim td on d.team_name = td.team_name
+	join player_dim bpd on d.bowler = bpd.player_name
+	join player_dim spd on d.batter = spd.player_name
+	join player_dim npsd on d.non_stricker = nspd.player_name
 where d.match_type_number = 4686;
 
 
-
-
-
-
-
-
-
-
-
-
+-- v3 add measurements
+select
+	d.match_type_number as match_id,
+	td.team_id, 
+	bpd.player_id as bowler_id, bpd.player_name,
+	spd.player_id as batter_id, spd.player_name,
+	nspd.player_id as non_stricker_id, nspd.player_name,
+	d.over, d.runs, d.extra_runs, d.extra_type
+from cricket.clean.delivery_clean_tbl d
+	join team_dim td on d.team_name = td.team_name
+	join player_dim bpd on d.bowler = bpd.player_name
+	join player_dim spd on d.batter = spd.player_name
+	join player_dim npsd on d.non_stricker = nspd.player_name
+where d.match_type_number = 4686;
+	
+	
+-- v4 remove unwanted columns
+select
+	d.match_type_number as match_id,
+	td.team_id, 
+	bpd.player_id as bowler_id, bpd.player_name,
+	spd.player_id as batter_id, spd.player_name,
+	nspd.player_id as non_stricker_id, nspd.player_name,
+	d.over, d.runs,
+	case when d.extra_runs is null then 0 else d.extra_runs end as extra_runs,
+	case when d.extra_type is null then 0 else d.extra_type end as extra_type,
+	case when d.player_out is null then 0 else d.player_out end as player_out,
+	case when d.player_out_kind is null then 0 else d.player_out_kind end as player_out_kind,
+from cricket.clean.delivery_clean_tbl d
+	join team_dim td on d.team_name = td.team_name
+	join player_dim bpd on d.bowler = bpd.player_name
+	join player_dim spd on d.batter = spd.player_name
+	join player_dim npsd on d.non_stricker = nspd.player_name
+where d.match_type_number = 4686;
 
 
 CREATE or replace TABLE delivery_fact (
